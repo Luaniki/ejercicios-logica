40a924274d4de958598ea86537a10512b05a2478
Luaniki <jorgeluis04avellacorrea@gmail.com>
2023-08-26 03:45:44 -0500

    primos

--- src/app/primos/primos.page.ts
+++ src/app/primos/primos.page.ts
@@ -22,4 +22,23 @@ export class PrimosPage implements OnInit {
     this.router.navigateByUrl('home');
   }
 
+  n!:number;
+
+  primos(){
+
+    for (let i = 1; i <= this.n; i++) {
+      let flag = 0;
+  
+      for (let j = 2; j < i; j++) {
+          if (i % j == 0) {
+              flag = 1;
+              break;
+          }
+      }
+  
+      if (i > 1 && flag == 0) {
+          alert(i);
+      }
+  }
+  }
 }
